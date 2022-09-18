fc50a1d15de0bf4c1536a89fe5975ca3704aad1d
skyrocket1000 <77110157+skyrocket1000@users.noreply.github.com>
2022-09-18 15:30:07 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1ee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your operator(+, -, * or /): ");
+            string userOp = Console.ReadLine();
+
+            Console.Write("Enter your second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(Calculator(num1, userOp, num2));
+
+            Console.WriteLine("Calculation Complete!");
+
+            Console.ReadLine();
+        }
+        static double Calculator(double num1, string userOp, double num2)
+        {
+            double result;
+
+            if (userOp == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (userOp == "-")
+            {
+                result = num1 - num2;
+            }
+            else if (userOp == "*")
+            {
+                result = num1 * num2;
+            }
+            else
+            {
+                result = num1 / num2;
+            }
+
+            return result;
+        }
+
+    }
+}
